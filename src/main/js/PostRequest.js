6c785357ffb3afe79bfe7ef67bcc37eb8773e169
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2020-05-14 20:19:14 -0600

    Adjustments - added PostRequest.js
    Signed-off-by: BuildTools <jumboofawarrior@gmail.com>

---
+++ src/main/js/PostRequest.js
@@ -0,0 +1,22 @@
+class PostRequest
+{
+    /*
+     * Java Script Version of PostRequest
+     * username;
+     * password;
+     * email;
+     * token;
+     * tokenUse;
+     * path;
+     * command;
+     * content;
+     * captchaCode;
+     *
+     */
+
+    constructor()
+    {
+
+    }
+
+}
